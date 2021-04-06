a85fdffa1366a4000d48296b8ba2333bbf527b23
Kaiser1612 <vinhtien1612@gmail.com>
2021-04-06 21:41:04 +0700

    done 1 rem 234

--- 3_4_2021_Lab_ex_string/main.cpp
+++ 3_4_2021_Lab_ex_string/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
-
+#include <string>
+#include <string.h>
+#include <cstring>
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+/*    string input="";
+    while (input!="***"){
+        getline(cin,input);
+        if (input == "***") break;
+        if (input[0]>input[1]){cout << "false"<< endl;}
+        else if (input[1]>input[2]){cout << "false"<<endl;}
+        else cout << "true" << endl;
+    }*/
+    
     return 0;
 }
