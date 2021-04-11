a4b85409cae297b9ea266ea0ab33736e4d64e89f
Kaiser1612 <vinhtien1612@gmail.com>
2021-04-11 23:49:27 +0700

    done_ex12_lab2_PF

--- lab_10_4_2021/main.cpp
+++ lab_10_4_2021/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
-
+#include <string>
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string str;
+    cin >> str;
+    int n = str.size();
+    int count=1;
+    int max=1;
+    for (int i = 1; i < n ;i++) {
+        if (str[i]==str[i-1] || toupper(str[i])==str[i-1] || tolower(str[i])==str[i-1]) count++;
+        else {
+            if (count > max) {
+                max = count;
+            }
+            count = 1;
+        }
+
+    }
+    if (count > max) {
+        max = count;
+    }
+    cout << max;
     return 0;
 }
