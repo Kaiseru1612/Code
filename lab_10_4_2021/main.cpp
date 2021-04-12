ddc279766073c7fea25fcd082ed4e2b4cafae62b
Kaiser1612 <vinhtien1612@gmail.com>
2021-04-12 16:52:37 +0700

    12_4_2021
    done lab_PF2 123 fail hidden case 3 rem 45

--- lab_10_4_2021/main.cpp
+++ lab_10_4_2021/main.cpp
@@ -1,8 +1,62 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <iomanip>
 using namespace std;
-int main() {
+
+
+void process(string fileName)   {
+    ifstream ifs;
+    ifs.open(fileName);
+    char c;
     string str;
+    int count=0;
+    getline(ifs,str);
+    int n = stoi(str);
+    double height[n];
+    double weight[n];
+    for (int i = 0; i < n; i++) {
+        height[i]=0;
+        weight[i]=0;
+    }
+    string heighttmp="";
+    string weighttmp="";
+    int i=0;
+    while (ifs.get(c)){
+
+        if (count == 3){
+                heighttmp+=c;
+        }
+        if (count == 4) {
+                height[i] = stod(heighttmp);
+                weighttmp += c;
+        }
+        if (c == '\n' || ifs.eof()){
+            weight[i]=stod(weighttmp);
+            count = 0;
+            i++;
+            heighttmp = "";
+            weighttmp = "";
+        }
+        if (c == ',') count++;
+    }
+    double sumh=0, sumw=0;
+    double aveh, avew;
+    for (int j = 0; j < n; j++) {
+        sumh += height[j];
+        sumw += weight[j];
+    }
+    int counth=0,countw=0;
+    for (int j = 0; j < n; j++) {
+        if (height[j] != 0) counth++;
+        if (weight[j] != 0) countw++;
+    }
+    aveh = sumh / counth;
+    avew = sumw / countw;
+    cout << setprecision(5) << aveh << " " << avew;
+}
+int main() {
+    /*string str;
     cin >> str;
     int n = str.size();
     int count=1;
@@ -20,6 +74,8 @@ int main() {
     if (count > max) {
         max = count;
     }
-    cout << max;
+    cout << max;*/
+    const char *filename = "test.txt";
+    process(filename);
     return 0;
 }
---
+++ untitled5/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+void createchessboard()
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
